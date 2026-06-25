7d4d5af6ca7580d43e4d2569e854438a6121f8e8
sjai5803 <sjai5803@gmail.com>
2026-06-25 14:23:31 +0530

    connection of external database

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 5432),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL 
+    ? { 
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false } // Required by most cloud providers
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
 
 module.exports = {
   pool,
